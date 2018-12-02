239249dc60c6e2772ebaf4c70881c6dcb18f8657
chris-thinkpad <cbrown184@protonmail.com>
2018-12-02 16:15:36 +0000

    remove unused method

--- src/main/java/config/Configurator.java
+++ src/main/java/config/Configurator.java
@@ -20,18 +20,4 @@ public class Configurator {
                 });
         return this;
     }
-    protected Configurator configure(JSONObject config, Class clazz) {
-        Arrays.stream(clazz.getDeclaredFields())
-                .filter(field -> field.getAnnotation(Inject.class) == null && field.getAnnotation(NonConfigurable.class) == null)
-                .forEach(field -> {
-                    try {
-                        field.setAccessible(true);
-                        field.set(this, config.get(field.getName()));
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                });
-        return this;
-    }
-
 }
